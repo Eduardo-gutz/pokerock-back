e77585d6cfa947c69fabe05447693c1da46dd25d
Eduardo-gutz <eduardogutzfl@gmail.com>
2022-12-18 22:29:20 -0600

    ADD: enable cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import enviroments from './enviroments';
 async function bootstrap() {
   const port = Number(enviroments.port);
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   await app.listen(port);
 }
 bootstrap();
